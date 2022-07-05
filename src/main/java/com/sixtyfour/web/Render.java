5ad724477b7f7842b7facd52c8cef18f1ad49b52
EgonOlsen <info@jpct.net>
2022-07-05 20:50:39 +0200

    Warning removed

--- src/main/java/com/sixtyfour/web/Render.java
+++ src/main/java/com/sixtyfour/web/Render.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sixtyfour.config.CompilerConfig;
 import com.sixtyfour.templating.Template;
 import com.sixtyfour.templating.TemplateManager;
 
